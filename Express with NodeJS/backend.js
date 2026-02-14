b04ca66bfda8b943dbf2610ac589f5412a4e671c
Francis-annor <annorfrancis55@gmail.com>
2026-02-14 13:08:02 +0000

    Sqlite3 database connection and some operations(creating table and inserting data) from the user

--- Express with NodeJS/backend.js
+++ Express with NodeJS/backend.js
@@ -1,4 +1,23 @@
 import express from 'express';
+import sqlite from 'sqlite3';
+
+const sqlite3 = sqlite.verbose();
+
+let db = new sqlite3.Database('./schooldb.sqlite', (err) => {
+    if(err){
+        console.error(err.message);
+    }
+    console.log('Connection to SQL Database Successful');
+});
+
+db.run(`CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email VARCHAR,
+    phone VARCHAR,
+    message VARCHAR
+    )`
+);
 
 const app = express();
 const port = 5500;
@@ -11,12 +30,12 @@ app.post('/submit', (req, res) =>{
     const email = req.body.email;
     const phone = req.body.tel;
     const message = req.body.message;
-    res.send(`
-        <h3>Name: ${name}</h3>
-        <p>Email: ${email}</p>
-        <p>Telephone: ${phone}</p>
-        <p>Message: ${message}</p>
-    `)
+
+    db.run(`INSERT INTO users(name, email, phone, message) VALUES(?,?,?,?)`,
+        [name, email, phone, message]
+    );
+
+    res.send(`Dear ${name}, Your message has successfully been received. We will get back to you within 48 hours.`)
 });
 
 app.listen(port, hostname, () => {
