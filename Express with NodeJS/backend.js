d825ff95ed0b28718ff7764b558f600661f118bd
Francis-annor <annorfrancis55@gmail.com>
2026-03-03 19:43:01 +0000

    Refactor backend.js to implement message insertion, update, and deletion functionalities with SQLite

--- Express with NodeJS/backend.js
+++ Express with NodeJS/backend.js
@@ -1,41 +1,94 @@
-import express from 'express';
+import express, { json } from 'express';
 import sqlite from 'sqlite3';
 
-const sqlite3 = sqlite.verbose();
+const sqlite3 = new sqlite.verbose();
 
-let db = new sqlite3.Database('./schooldb.sqlite', (err) => {
+let sql;
+
+let db = new sqlite3.Database('./messagedb.sqlite', sqlite3.OPEN_READWRITE, (err) => {
     if(err){
         console.error(err.message);
     }
     console.log('Connection to SQL Database Successful');
 });
 
-db.run(`CREATE TABLE IF NOT EXISTS users(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    email VARCHAR,
-    phone VARCHAR,
-    message VARCHAR
-    )`
-);
+// sql = `CREATE TABLE IF NOT EXISTS messages(
+// id INTEGER PRIMARY KEY AUTOINCREMENT,
+// name TEXT,
+// email VARCHAR,
+// phone VARCHAR,
+// message VARCHAR)`;
+
+// db.run(sql, [], (err) =>{
+//     if(err) return console.log(err.message);
+//     console.log(`Message Table created successfully`);
+// });
 
 const app = express();
 const port = 5500;
 const hostname = '127.0.0.1';
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
 
 app.post('/submit', (req, res) =>{
-    const name = req.body.fullname;
+    const id = req.body.id;
+    const name = req.body.name;
     const email = req.body.email;
     const phone = req.body.tel;
     const message = req.body.message;
 
-    db.run(`INSERT INTO users(name, email, phone, message) VALUES(?,?,?,?)`,
-        [name, email, phone, message]
-    );
+    if(id == null){
+        sql = `INSERT INTO messages(name, email, phone, message) VALUES(?,?,?,?)`
+        db.run(sql,
+            [name, email, phone, message],
+            (err) =>{
+                if(err) return console.error(err.message);
+                console.log(`Data Insertion Successful`);
+            }
+        );
+    }else if(id != null){
+        sql =  `UPDATE messages SET name = ?, email = ?, phone = ?, message = ? WHERE id = ?`;
+        db.run(
+            sql,
+            [name, email, phone, message, id],
+            (err) => {
+                if(err) return console.error(err.message);
+                console.log('Message Updated Successfully');
+            }
+        );
+    }
+});
 
-    res.send(`Dear ${name}, Your message has successfully been received. We will get back to you within 48 hours.`)
+app.get('/api/messages', (req, res) => {    
+    sql = `SELECT * FROM messages`;
+    db.all(
+        sql,
+        [],
+        (err, rows) => {
+        if(err) return console.error(err.message);
+        // rows.forEach(row => {
+        //     console.log(row);
+        // });
+        // for(let r in rows){
+        //     console.log(r);
+        // }
+        res.json(rows);
+    });
+});
+
+app.post('/delete', (req, res) =>{
+    const id = req.body.id;
+    sql = `DELETE FROM messages WHERE id = ?`;
+    db.run(
+        sql,
+        [id],
+        (err) => {
+            if (err) return console.error(err.message);
+            console.log('Message Deleted Successfully');
+        }
+    );
 });
 
 app.listen(port, hostname, () => {
