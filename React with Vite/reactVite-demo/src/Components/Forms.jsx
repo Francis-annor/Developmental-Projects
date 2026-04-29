d1ff3b0cb6016a64782ebbee6283051d3bfb932d
Francis-Annor <annorfrancis55@gmail.com>
2026-04-29 19:32:52 +0000

    feat: Enhance Login component with state management for name, message, and marital status; implement form submission handling

--- React with Vite/reactVite-demo/src/Components/Forms.jsx
+++ React with Vite/reactVite-demo/src/Components/Forms.jsx
@@ -10,20 +10,47 @@ function Forms(){
 
 // React Forms
 function Login(){
-    const [name, setName] = useState("");
+    const [name, setName] = useState("Mr. ");
+    const [message, setMessage] = useState("");
+    const [selected, setOption] = useState("Divorced");
 
+    // Controlling Components
     // const handleChange = (e) => {
-    //     setName(e.target.name);
+    //     setName(e.target.value);
+    // }
+
+    // Handling Form Submit and preventing Default
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     alert(name);
     // }
 
     return (
-
-        <form>
+    // Handling Form Submit and preventing Default
+        <form onSubmit={ (e) => {e.preventDefault(); alert(name +' '+ message)} }>
             <h3>Login</h3>
             <label>Enter your name: 
                 <input type='text' value={ name } onChange={ (e) => setName(e.target.value) } />
             </label>
-            <p>Current value: { name }</p>
+            <p>Name: { name }</p>
+            <label>Marital Status: 
+                <select 
+                value={ selected } 
+                onChange={(e) => setOption(e.target.value)}>
+                    <option value='Single'>Single</option>
+                    <option value='Married'>Married</option>
+                    <option value='Widow'>Widow</option>
+                    <option value='Separated'>Separated</option>
+                </select>
+            </label>
+            <br />
+            <label>Write here: 
+                <textarea 
+                value={ message } 
+                onChange={ (e) => setMessage(e.target.value)} />
+            </label>
+            <pre>Message: { message }</pre>
+            <input type='submit' />
         </form>
 
     )
