bc1bd48dcd9efb8975fdfe92ecdc41462dabaae0
Francis-Annor <annorfrancis55@gmail.com>
2026-05-06 13:27:01 +0000

    refactor: Introducing styled-components. Rename Keyboard component to Keys and update structure for better readability

--- React with Vite/reactVite-demo/src/Components/Keyboard.jsx
+++ React with Vite/reactVite-demo/src/Components/Keyboard.jsx
@@ -1,37 +1,75 @@
-function Keyboard(){
-    <>
-    <AlphaNumericKeys>A - Z</AlphaNumericKeys>
-    <FunctionalKeys>F1 - F12</FunctionalKeys>
-    <NumericKeys>0 - 9</NumericKeys>
-    <ArrowKeys>Top, Down, Left, Right</ArrowKeys>
-    <EditKeys>Insert, Delete, Page up, Page Down, End, Scroll, Home, Print screen, Pause, Break, System request</EditKeys>
-    </>
-}
+// CSS Styled Components
+import styled from 'styled-components';
 
-function AlphaNumericKeys(props){
+// Using CSS-in-JS
+const AlphaNumericKeys = styled.h4`
+    background-color: red;
+    color: white;
+    padding: 10px 20px;
+`;
+
+// Props in styled components
+const H2 = styled.h2`
+    background: ${props => props.sameColor === 'orange' ? 'black' : 'orange'};
+    color: white;
+    padding: 10px 20px;
+`;
+
+// Another way of letting multiple elements have the same styles is to extend existing styled components.
+const TwoH2 = styled.h3`
+    padding: 10px 20px;
+    color: white;
+`;
+
+const FirstH2 = styled(TwoH2)`
+    background: dodgerblue;
+`;
+
+const SecondH2 = styled(TwoH2)`
+    background: gray;
+`;
+
+function Keys(props){
     return (
-        <h4>{props.children}</h4>
+        <>
+        <h1>Keyboard</h1>
+        <AlphaNumericKeys>A - Z</AlphaNumericKeys>
+        <FunctionalKeys>{props.children}</FunctionalKeys>
+        <NumericKeys>{props.children}</NumericKeys>
+        <ArrowKeys>{props.children}</ArrowKeys>
+        <EditKeys>{props.children}t</EditKeys>
+        </>
     )
 }
 
-function FunctionalKeys(){
+// function AlphaNumericKeys(){
+//     return (
+//         <h4>A - Z</h4>
+//     )
+// }
 
+function FunctionalKeys(){
+    return (
+        <H2 sameColor='orange'>F1 - F12</H2>
+    )
 }
 
 function NumericKeys(){
-
-}
-
-function ControlKeys(){
-
+    return (
+        <H2>0 - 9</H2>
+    )
 }
 
 function ArrowKeys(){
-
+    return (
+        <FirstH2>Top, Down, Left, Right</FirstH2>
+    )
 }
 
 function EditKeys(){
-
+    return (
+        <SecondH2>Insert, Delete, Page up, Page Down, End, Scroll, Home, Print screen, Pause, Break, System request</SecondH2>
+    )
 }
 
-export default Keyboard
+export default Keys
